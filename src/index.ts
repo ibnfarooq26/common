c0fcebbb652c9c8ebdd9fb495f225428e5483549
ibnfarooq26 <85802553+ibnfarooq26@users.noreply.github.com>
2022-11-16 10:02:46 +0000

    second commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+const color: Color = {
+  red: 1,
+  blue: 2,
+  green: 3,
+};
+
+console.log(color, "hello");
+
+export default color;
